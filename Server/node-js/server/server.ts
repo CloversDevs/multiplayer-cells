f2fbe6235f5c74613c5992a7fa603ac89d6e5e52
Uribe Facundo <cloversdevs@gmail.com>
2025-03-09 00:47:26 -0300

    Compile both client and server typescript (allegedly); Use public name for docker-compose volume

--- Server/node-js/server/server.ts
+++ Server/node-js/server/server.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);

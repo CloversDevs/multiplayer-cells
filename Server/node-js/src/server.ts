7eefc6cf421090879564adbe6849e105a223ee5e
Uribe Facundo <cloversdevs@gmail.com>
2025-03-08 22:01:25 -0300

    Move page content to public folder

--- Server/node-js/src/server.ts
+++ Server/node-js/src/server.ts
@@ -1,16 +1,12 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the "html" directory
-app.use(express.static(path.join(__dirname, '../html')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html', 'index.html'));
-});
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });

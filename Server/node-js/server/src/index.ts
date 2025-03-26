b7432d8e363dbbeb659066edace22fbee26e3cdd
Uribe Facundo <cloversdevs@gmail.com>
2025-03-26 16:39:39 -0300

    Move and properly mount public files folder to /Server/node-js/public/; Use debug https certificates for serving Godot html export

--- Server/node-js/server/src/index.ts
+++ Server/node-js/server/src/index.ts
@@ -1,14 +1,26 @@
 import express from "express";
 import path from "path";
+import https from "https";
+import fs from "fs";
+import serveIndex from "serve-index";
 
 const app = express();
-const PORT = 3000;
+const PORT = 443;
 
-// Serve static files from the "public" directory
+// Serve static files from the compiled frontend directory
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+// Serve static files from the "public" directory
+// To only host files without file explorer: app.use(express.static(path.join(__dirname, "../../public")));
+app.use("/public", express.static(path.join(__dirname, "../../public")), serveIndex(path.join(__dirname, "../../public"), { icons: true }));
 
+// Certificates for HTTPS.
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "../certs/server.key")),
+  cert: fs.readFileSync(path.join(__dirname, "../certs/server.cert")),
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Secure server running at https://localhost:${PORT}`);
   console.log("Hi!");
 });
